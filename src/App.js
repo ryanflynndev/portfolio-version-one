19a602dd79b12f53624fabbf6e048e6ac7bea445
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-04 14:35:17 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
-  return (
-    <div className="App">
 
+  return (
+    <div>
+      <Navbar/>
     </div>
   );
 }
